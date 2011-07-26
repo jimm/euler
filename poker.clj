1e60b7a2b6a03a5e8ca27008726fa73952cb8bec
Jim Menard <jim@jimmenard.com>
2011-07-26 11:06:27 -0400

    p54 solution

--- poker.clj
+++ poker.clj
@@ -43,7 +43,7 @@
 ;;
 ;; How many hands does Player 1 win?
 
-(def debug println)
+;(def debug println)
 (def debug list)
 
 (defstruct card :value :suit)
@@ -51,19 +51,26 @@
 (def *card-vals* (vec "..23456789TJQKA"))
 
 (defn make-card
+  "Given a single card string like \"8H\", returns a card structure."
   [s]
   (struct card (.indexOf *card-vals* (first s)) (second s)))
 
 (defn make-hand
-  "Used only during testing/development."
+  "Pass this function a string like \"8H 2S AC JS JD\" and it returns a
+sequence of card structures. Used only during testing/development. "
   [s]
   (map make-card (.split s " ")))
+
+(defn hand-str
+  "Used only during testing/development."
+  [hand]
+  (.trim (apply str (interleave (sort (map #(str (nth *card-vals* (:value %)) (:suit %)) hand)) (repeat " ")))))
 
 ;; Hand-rank stores information about a hand's value: its rank number from 0
 ;; (simple highest value card) through 9 (royal flush), its hand-val (the
 ;; value of the highest pair, three of a kind, highest in straight or flush,
 ;; full house, etc.), and the highest card in the hand.
-(defstruct poker-hand-rank :hand-rank :hand-val :highest-card)
+(defstruct hand-rank :name :hand-rank :hand-val :highest-card)
 
 (defn all-same-suit?
   [hand]
@@ -71,7 +78,8 @@
 
 (defn straight-hand?
   [vals]
-  (= 4 (- (apply max vals) (apply min vals))))
+  (let [min-val (apply min vals)]
+    (= (range min-val (+ min-val 5)) (sort vals))))
 
 (defn hand-vals
   [hand]
@@ -92,81 +100,74 @@
 
 (defn poker-royal-flush?
   [hand]
-  (debug "poker-royal-flush?") ; DEBUG
   (when (and (all-same-suit? hand)
              (= #{10 11 12 13 14} (set (hand-vals hand))))
-    (struct poker-hand-rank 9 nil 14)))
+    (struct hand-rank "royal flush" 9 nil 14)))
 
 (defn poker-straight-flush?
   [hand]
-  (debug "poker-straight-flush?") ; DEBUG
   (let [vals (hand-vals hand)]
     (when (and (all-same-suit? hand)
                (straight-hand? vals))
       (let [max-val (apply max vals)]
-        (struct poker-hand-rank 8 max-val max-val)))))
+        (struct hand-rank "straight flush" 8 max-val max-val)))))
 
 (defn poker-four-of-a-kind?
   [hand]
-  (debug "poker-four-of-a-kind?") ; DEBUG
   (let [freqs (hand-freqs hand)
         found-four (first (filter #(= 4 (val %)) freqs))]
     (when found-four
-      (struct poker-hand-rank 7 (key found-four) (highest-val hand)))))
+      (struct hand-rank "four of a kind" 7 (key found-four) (highest-val hand)))))
 
 (defn poker-full-house?
   [hand]
-  (debug "poker-full-house?") ; DEBUG
   (let [freqs (hand-freqs hand)
         freq-vals (map val freqs)
         found-three (first (filter #(= 3 (val %)) freqs))]
     (when (and (some #{3} freq-vals)
                (some #{2} freq-vals))
-      (struct poker-hand-rank 6 (key found-three) (highest-val hand)))))
+      (struct hand-rank "full house" 6 (key found-three) (highest-val hand)))))
 
 (defn poker-flush?
   [hand]
-  (debug "poker-flush?") ; DEBUG
   (when (all-same-suit? hand)
     (let [hv (highest-val hand)]
-      (struct poker-hand-rank 5 hv hv))))
+      (struct hand-rank "flush" 5 hv hv))))
 
 (defn poker-straight?
   [hand]
-  (debug "poker-straight?") ; DEBUG
   (when (straight-hand? (hand-vals hand))
     (let [hv (highest-val hand)]
-      (struct poker-hand-rank 4 hv hv))))
+      (struct hand-rank "straight" 4 hv hv))))
 
 (defn poker-three-of-a-kind?
   [hand]
-  (debug "poker-three-of-a-kind?") ; DEBUG
   (let [freqs (hand-freqs hand)
-        freq-vals (map val freqs)
-        found-three (first (filter #(= 3 (val %)) freqs))]
-    (when (some #{3} (hand-freq-vals hand))
-      (struct poker-hand-rank 3 (key found-three) (highest-val hand)))))
+        grouped (group-by #(= 3 (val %)) freqs)] ; {true [three-freq] false [not-three-freqs]}
+    (when (get grouped true)
+      (struct hand-rank "three of a kind" 3
+              (ffirst (get grouped true))
+              (apply max (map first (get grouped false)))))))
 
 (defn poker-two-pairs?
   [hand]
-  (debug "poker-two-pairs?") ; DEBUG
   (let [freqs (hand-freqs hand)
-        pairs (filter #(= 2 (val %)) freqs)]
-    (debug "freqs =" freqs)
-    (debug "pairs =" pairs)
-    (when (= 2 (count pairs))
-      (struct poker-hand-rank 2 (apply max (map first pairs)) (highest-val hand)))))
+        grouped (group-by #(= 2 (val %)) freqs)] ; {true [pair-freqs] false [not-pair-freqs]}
+    (when (= 2 (count (get grouped true)))
+      (struct hand-rank "two pairs" 2
+              (apply max (map first (get grouped true)))
+              (apply max (map first (get grouped false)))))))
 
 (defn poker-one-pair?
   [hand]
-  (debug "poker-one-pair?") ; DEBUG
-  (let [freqs (hand-freqs hand)]
-    (when (some #{2} (map val freqs))
-      (struct poker-hand-rank 1 (key (first (filter #(= 2 (val %)) freqs))) (highest-val hand)))))
+  (let [freqs (hand-freqs hand)
+        grouped (group-by #(= 2 (val %)) freqs)] ; {true [pair-freq] false [not-pair-freqs]}
+    (when (get grouped true)
+      (struct hand-rank "one pair" 1 (ffirst (get grouped true))
+              (apply max (map first (get grouped false)))))))
 
 (defn rank-poker-hand
   [hand]
-  (debug "rank-poker-hand") ; DEBUG
   (or (poker-royal-flush? hand)
       (poker-straight-flush? hand)
       (poker-four-of-a-kind? hand)
@@ -176,23 +177,26 @@
       (poker-three-of-a-kind? hand)
       (poker-two-pairs? hand)
       (poker-one-pair? hand)
-      (struct poker-hand-rank 0 nil (highest-val hand))))
+      (struct hand-rank "simple high card" 0 nil (highest-val hand))))
 
 (defn player-1-wins-tie-breaker?
+  "Breaks ties in the case of equal hand rank values. Looks first at hand
+  value then if those are equal looks at highest card."
   [rank1 rank2]
   (debug "player-1-wins-tie-breaker?")
-  (debug rank1)
-  (debug rank2)
   (if (and (:hand-val rank1) (:hand-val rank2))
-    (> (:hand-val rank1) (:hand-val rank2))
+    (if (= (:hand-val rank1) (:hand-val rank2))
+      (> (:highest-card rank1) (:highest-card rank2))
+      (> (:hand-val rank1) (:hand-val rank2)))
     (> (:highest-card rank1) (:highest-card rank2))))
 
 (defn player-1-wins?
   [hand1 hand2]
+  (debug "player-1-wins?")
   (let [rank1 (rank-poker-hand hand1)
         rank2 (rank-poker-hand hand2)]
-    (debug "hand1 =" hand1) (debug "rank1 =" rank1) ; DEBUG
-    (debug "hand2 =" hand2) (debug "rank2 =" rank2) ; DEBUG
+    (debug "hand1 =" (hand-str hand1) "rank1 =" rank1) ; DEBUG
+    (debug "hand2 =" (hand-str hand2) "rank2 =" rank2) ; DEBUG
     (cond (> (:hand-rank rank1) (:hand-rank rank2)) true
           (< (:hand-rank rank1) (:hand-rank rank2)) false
           true (player-1-wins-tie-breaker? rank1 rank2))))
@@ -204,5 +208,31 @@
                 :let [cards (.split hand " ")
                       p1 (map make-card (take 5 cards))
                       p2 (map make-card (take 5 (drop 5 cards)))]]
-            (if (player-1-wins? p1 p2) 1 0))))
+            (if (player-1-wins? p1 p2)
+              (do
+                (debug "winner: player 1" (hand-str p1) "-" (hand-str p2))
+                1)
+              (do
+                (debug "winner: player 2" (hand-str p1) "-" (hand-str p2))
+                0)))))
 
+;; ================ testing ================
+
+(defstruct test-hand :hand1 :hand2 :winner)
+
+(defn test-hands
+  []
+  (let [f #(struct test-hand (make-hand %1) (make-hand %2) %3)
+        test-hands
+        (list
+         (f "5H 5C 6S 7S KD" "2C 3S 8S 8D TD" 2)
+         (f "5D 8C 9S JS AC" "2C 5C 7D 8S QH" 1)
+         (f "2D 9C AS AH AC" "3D 6D 7D TD QD" 2)
+         (f "4D 6S 9H QH QC" "3D 6D 7H QD QS" 1)
+         (f "2H 2D 4C 4D 4S" "3C 3D 3S 9S 9D" 1)
+         (f "2H 3H 4H 5C 6H" "3S 5S 6S AS QH" 1))]
+    (doseq [hand test-hands]
+      (let [winner (if (player-1-wins? (:hand1 hand) (:hand2 hand)) 1 2)]
+        (if (= winner (:winner hand)) (print ".")
+            (print "E"))))
+    (println)))
