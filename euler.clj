ec29c8ee712d74ba31fc5b6fac0aa94aee3fe7b6
Jim Menard <jim@jimmenard.com>
2011-07-26 15:28:37 -0400

    p57 solved

--- euler.clj
+++ euler.clj
@@ -1737,3 +1737,39 @@ satisfies this problem's criteria."
            (for [a (range 1 100)
                  b (range 1 100)]
              (sum-digits (expt a b))))))
+
+;; ================
+
+;; It is possible to show that the square root of two can be expressed as an
+;; infinite continued fraction.
+;;
+;;  2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+;;
+;; By expanding this for the first four iterations, we get:
+;;
+;; 1 + 1/2 = 3/2 = 1.5
+;; 1 + 1/(2 + 1/2) = 7/5 = 1.4
+;; 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+;; 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+;;
+;; The next three expansions are 99/70, 239/169, and 577/408, but the eighth
+;; expansion, 1393/985, is the first example where the number of digits in
+;; the numerator exceeds the number of digits in the denominator.
+;;
+;; In the first one-thousand expansions, how many fractions contain a
+;; numerator with more digits than denominator?
+
+(defn p57
+  []
+  (loop [expansion 0
+         frac-part 0
+         num-big-numerator 0]
+    (if (>= expansion 1000)
+      num-big-numerator
+      (let [new-frac-part (/ 1 (+ 2 frac-part))
+            new-num (inc new-frac-part)]
+        (recur (inc expansion)
+               new-frac-part
+               (if (> (count (str (numerator new-num))) (count (str (denominator new-num))))
+                 (inc num-big-numerator)
+                 num-big-numerator))))))
