f3b67ec83416c76eb8a04a77aab62674aafa8ad4
Jim Menard <jim@jimmenard.com>
2011-08-02 09:16:11 -0400

    note about possible caching of score image in README

--- euler.clj
+++ euler.clj
@@ -2065,3 +2065,13 @@ satisfies this problem's criteria."
   (def tris    (p61-in-range triangle-numbers)))
   (def interesting (set (concat octs hepts hexes pents squares tris)))
 )
+
+;; ================
+
+;; The cube 41063625 (345^3), can be permuted to produce two other cubes:
+;; 56623104 (384^3) and 66430125 (405^3). In fact, 41063625 is the smallest
+;; cube which has exactly three permutations of its digits which are also
+;; cubes.
+;;
+;; Find the smallest cube for which exactly five permutations of its digits
+;; are cubes.
