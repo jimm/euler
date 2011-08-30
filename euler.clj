1c632246773e62eb9f01b7459edaff6a4f1255fe
Jim Menard <jim@jimmenard.com>
2011-08-30 06:57:14 -0400

    p71 solution

--- euler.clj
+++ euler.clj
@@ -2406,3 +2406,46 @@ problem description."
 (defn p70
   []
   (apply min-key second (p70-ns-and-nphis)))
+
+;; ================
+
+;; Consider the fraction n/d, where n and d are positive integers. If n<d
+;; and HCF(n,d)=1, it is called a reduced proper fraction.
+;; 
+;; If we list the set of reduced proper fractions for d <= 8 in ascending
+;; order of size, we get:
+;;
+;; 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8,
+;; 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+;;
+;; It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+;;
+;; By listing the set of reduced proper fractions for d <= 1,000,000 in
+;; ascending order of size, find the numerator of the fraction immediately
+;; to the left of 3/7.
+
+
+;; See http://en.wikipedia.org/wiki/Farey_Sequence
+;;
+;; If a/b and c/d are neighbors in a Farey seq, with a/b < c/d, then
+;;
+;;   bc - ad = 1
+;;
+;; Remember that for this problem c = 3 and d = 7, so
+;;
+;;   3b - 7a = 1
+;;
+;; Conversely, if the previous is true then the two are neighbors in the
+;; Farey sequence of order max(b, d). Now all we need to do is find the
+;; highest a where b is an integer.
+;;
+;; (bc - 1) / d = a
+;; (3b - 1) / 7 = a
+
+(defn p71
+  []
+  ;; Increase numerator to max value, then reduce numer by 1.
+  (loop [b 1000000]
+    (let [a (/ (dec (* 3 b)) 7)]
+      (cond (integer? a) a
+            true (recur (dec b))))))
